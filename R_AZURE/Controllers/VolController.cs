6b296fe59dbf54d6eb92c121084321c933280455
Josué Drack <joss.drack@gmail.com>
2019-04-22 20:21:49 +0200

    Model Vol

--- R_AZURE/Controllers/VolController.cs
+++ R_AZURE/Controllers/VolController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using R_AZURE.Models;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -13,7 +14,13 @@ namespace R_AZURE.Controllers
         // GET: /<controller>/
         public IActionResult Index()
         {
-            return View();
+            //VolModel [] VolModel = new VolModel();
+            var VolModel = new VolModel("D455G","Paris","Porto Seguro", new DateTime(2019, 6, 1, 7, 47, 0), new DateTime(2019, 6, 1, 7, 47, 0),1500);
+           
+
+
+
+            return View(VolModel);
         }
 
         //Afficher vol par id
---
+++ R_AZURE/Models/VolModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+namespace R_AZURE.Models
+{
+    public class VolModel
+    {
+        public String numVol {get; set;}
+        public String depart { get; set; }
+        public String arriver { get; set; }
+        public DateTime heureDepart { get; set; }
+        public DateTime heureArriver { get; set; }
+        public int place { get; set; }
+
+
+        //public VolMode();
+        public VolModel(String num, String d, String a,DateTime timeD, DateTime timeA, int p )
+        {
+            this.numVol = num;
+            this.depart = d;
+            this.arriver = a;
+            this.heureDepart = timeD;
+            this.heureArriver = timeA;
+            this.place = p;
+
+        }
+
+        public VolModel(String num,String d, String a)
+        {
+            this.numVol = num;
+            this.depart = d;
+            this.arriver = a;
+        }
+    }
+}
